010e61abfd5bda1650d23776a7bf9bea6a3e9423
Joshua Fairchild and Marilyn Carlin <joshua+marilyn@>
2017-08-02 09:39:10 -0700

    add/adjust links and content to make the website a little more intuitive/user friendly

--- src/TravelBlog/Models/Location.cs
+++ src/TravelBlog/Models/Location.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TravelBlog.Models
 {
-        [Table("Locations")]
+    [Table("Locations")]
     public class Location
     {
         public Location()

19b4cc6531eb948aebf1fa6b59927602145896f3
Joshua Fairchild and Marilyn Carlin <joshua+marilyn@>
2017-08-02 11:04:43 -0700

    add a .ThenInclude() statement to location details that will load the people related to each location

---
+++ src/TravelBlog/Migrations/20170802174448_RemovePeoplePropertyFromLocation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelBlog.Migrations
+{
+    public partial class RemovePeoplePropertyFromLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_People_Locations_LocationId",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "IX_People_LocationId",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "People");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_LocationId",
+                table: "People",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_People_Locations_LocationId",
+                table: "People",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
